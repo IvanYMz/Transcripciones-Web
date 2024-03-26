773c8d8b73d1fefff89b921622e1f2041c4d409d
Ivan Morales <ivanymz09@gmail.com>
2024-03-26 11:40:03 -0600

    .env para las credenciales de supabase

--- services/Supabase/connection.ts
+++ services/Supabase/connection.ts
@@ -1,7 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 
-///////////////////////SUPABASE KEYS/////////////////////////////////
-const supabaseUrl = 'https://vvqnwdlbuuuqutjpiwat.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ2cW53ZGxidXV1cXV0anBpd2F0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA0NjI4MTIsImV4cCI6MjAyNjAzODgxMn0.u1CWVoTu_2tWJLuawl3Jir8IxqSO5n4fQOp9GDr_7rU';
+const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL;
+const supabaseKey = import.meta.env.PUBLIC_SAPABASE_ANON_KEY;
 export const supabase = createClient(supabaseUrl, supabaseKey);
-////////////////////////SUPABASE KEYS/////////////////////////////////
